34a5618cf9329242bf1e3a8f376402b0e2b58887
Murilo Ortiz_dev <muriloortizv10@gmail.com>
2026-04-30 19:05:57 -0300

    Projeto 2.0 .py
    Projeto atualizado com robustez de código, ou seja, agora esta tratando alguns possíveis erros dentro dele, e retornando mensagens amigaveis.

--- projeto_da_disciplina.py
+++ projeto_da_disciplina.py
@@ -5,59 +5,120 @@ print('LIST para listar todos os projetos.')
 print('REMEMBER para relembrar os comandos')
 print('UPDATE para atualizar um projeto.')
 print('DELETE para remover um projeto')
+
 import datetime
 import json
-ARQUIVO = "dados.json"
+ARQUIVO = 'dados.json'
 
 def carregar_dados():
     try:
-        with open(ARQUIVO, "r") as f:
+        with open(ARQUIVO, 'r') as f:
             return json.load(f)
-    except:
+    except FileNotFoundError:
+        print('Se é sua primeira vez abrindo o programa, adicione um projeto para criar um arquivo para salvar os projetos.')
+        return []
+    except json.JSONDecodeError:
+        print('Arquivo vazio ou corrompido!')
         return []
 
 def salvar_dados(lista):
-    with open(ARQUIVO, "w") as f:
-        json.dump(lista, f, indent=4, default=str)
+    try:
+        with open(ARQUIVO, 'w') as f:
+            return json.dump(lista, f, indent=4)
+    except FileNotFoundError:
+        print('Se é sua primeira vez abrindo o programa, adicione um projeto para criar um arquivo para salvar os projetos.')
+        return []
+    except json.JSONDecodeError:
+        print('Arquivo vazio ou corrompido!')
+        return []
 
 lista = carregar_dados()
 
 while True:
     def procurar_projeto_lista(nome, lista):
+        try:
+            for projeto in lista:
+                if projeto['nome'] == nome:
 
-        for projeto in lista:
-            if projeto['nome'] == nome:
-
-                return projeto
+                    return projeto
+        except:
+            print('Projeto não foi encontrado, digite o nome de um projeto existente.')
 
     def adicionar_projeto():
-        quantidade_projetos = int(input('Informe quantos projetos gostaria de cadastrar:'))
+        try:
+            quantidade_projetos = int(input('Informe quantos projetos gostaria de cadastrar:'))
+            if quantidade_projetos <= 0:
+                print('Quantidade digitada invalida, tente novamente.')
+            else:
+                for i in range(quantidade_projetos):
+                    nome_projeto = input('Digite o nome do projeto:')
+                    print('Parabéns, novo projeto cadastrado!')
+                    cadastro_projeto = {
+                        'nome': (f'{nome_projeto}'),
+                        'status': False,
+                        'historico': []
+                    }
 
-        if quantidade_projetos <= 0:
-            print('Quantidade digitada invalida, tente novamente.')
-        else:
-            for i in range(quantidade_projetos):
-                nome_projeto = input('Digite o nome do projeto:')
-                print('Parabéns, novo projeto cadastrado!')
-                cadastro_projeto = {
-                    'nome': (f'{nome_projeto}'),
-                    'status': False,
-                    'historico': []
-                }
+                    lista.append(cadastro_projeto)
+                salvar_dados(lista)
+
+        except ValueError:
+            print('Formato digitado inválido, tente novamente!')
 
 
-                lista.append(cadastro_projeto)
-        salvar_dados(lista)
 
     def listar_projetos():
-        if lista == []:
-            print('Não há nenhum projeto registrado no momento, digite o comando ADD para adicionar um projeto.')
-        else:
+        try:
             for elemento in lista:
                 print('Detalhes do projeto:')
                 for chave, valor in elemento.items():
                     print(f'{chave}: {valor}')
                 print()
+        except:
+            print('Não há nenhum projeto registrado no momento, digite o comando ADD para adicionar um projeto.')
+
+
+    def atualizar_projetos():
+        listar_projetos()
+        nome_projeto_modificar = input('Qual projeto deseja modificar?')
+        projeto_modificar = procurar_projeto_lista(nome_projeto_modificar, lista)
+
+        if projeto_modificar is not None:
+            nome = input('Digite o novo nome do projeto:')
+            try:
+                status = bool(int(input('Finalizou o projeto? Digite 1 para sim e 0 para não')))
+            except ValueError:
+                print('Formato digitado inválido, tente novamente')
+
+            projeto_modificar['nome'] = nome
+            projeto_modificar['status'] = status
+
+            data_mudanca = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            entrada_atualizacao = (
+                projeto_modificar['historico'].append(data_mudanca),
+                projeto_modificar['nome'],
+                projeto_modificar['status']
+            )
+            salvar_dados(lista)
+
+
+
+        else:
+            print('Não encontrei o projeto, sem projetos para atualizar ou nome do projeto digitado errado.')
+
+    def deletar_projetos():
+        listar_projetos()
+        nome_projeto_remover = input('Qual projeto deseja remover?')
+        projeto_remover = procurar_projeto_lista(nome_projeto_remover, lista)
+        if projeto_remover is not None:
+            lista.remove(projeto_remover)
+            print(f"projeto {projeto_remover['nome']} removido com sucesso!")
+            salvar_dados(lista)
+
+
+        else:
+            print('Não encontrei o projeto, sem projetos para deletar ou nome do projeto digitado errado.')
 
     def relembrar_comandos():
         print('ABOUT para informações.')
@@ -71,9 +132,9 @@ while True:
     def sobre_usuario():
         print('Este é o Gestor de Portfólio do Murilo')
 
-
     comando = input('Digite um comando:')
     comando = comando.upper()
+
     if comando == 'ABOUT':
         sobre_usuario()
 
@@ -81,7 +142,8 @@ while True:
         relembrar_comandos()
 
     elif comando == 'QUIT':
-        print('Saindo do Gestor de Portfólio...Até logo!') 
+        print('Saindo do Gestor de Portfólio...Até logo!')
+        salvar_dados(lista)
         break
 
     elif comando == 'ADD':
@@ -93,35 +155,11 @@ while True:
 
 
     elif comando == 'UPDATE':
-        nome_projeto_modificar = input('Qual projeto deseja modificar?')
-        projeto_modificar = procurar_projeto_lista(nome_projeto_modificar, lista)
-
-        if projeto_modificar is not None:
-            nome = input('Digite o novo nome do projeto:')
-            status = bool(input('Finalizou o projeto? Digite 1 para sim e 0 para não'))
-            projeto_modificar['nome'] = nome
-            projeto_modificar['status'] = status
-
-            data_mudanca = datetime.datetime.now()
-            entrada_atualizacao = (data_mudanca,projeto_modificar['nome'], projeto_modificar['status'])
-            projeto_modificar['historico'].append(entrada_atualizacao)
-            salvar_dados(lista)
-
-
-        else:
-            print('Não encontrei o projeto')
+        atualizar_projetos()
 
 
     elif comando == 'DELETE':
-        nome_projeto_remover = input('Qual projeto deseja remover?')
-        projeto_remover = procurar_projeto_lista(nome_projeto_remover, lista)
-        if projeto_remover is not None:
-            lista.remove(projeto_remover)
-            print(f"projeto {projeto_remover['nome']} removido com sucesso!")
-
-        else:
-            print('Não encontrei o projeto')
-
+        deletar_projetos()
 
     else:
-        print('ERRO: Comando desconhecido')
+        print('ERRO: Comando desconhecido. Tente um comando válido!')
